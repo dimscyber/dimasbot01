6e3f8ae6e6c96f8f6079fcf1b35aebf7361dff37
dimscyber <60718484+dimscyber@users.noreply.github.com>
2021-02-03 17:39:06 +0700

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 5K > Akses Fitur ViP
+-Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_wa.me/6285722553869 atau ketik *${prefix}owner*
+
+*NOTE*
+
+*GRUP WHATSAPP BOT :*
+_https://chat.whatsapp.com/DSSHmG2KjKJLoFp9B9mkVs_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/help.js
@@ -0,0 +1,100 @@
+const help = (prefix) => { 
+	return `
+╭────┈ ↷
+┃□│✎┊NOEERBOT
+┃□│╭────────╯
+┃□││❀:ཻུ۪۪⸙Nama : NOEERBOT
+┃□││❀:ཻུ۪۪⸙Anuthor : Aris187 ID
+┃□││❀:ཻུ۪۪⸙IG : @_sadboy.ig
+┃□││❀:ཻུ۪۪⸙prefix : ${prefix}
+╰─────────────────┈ ❁ཻུ۪۪⸙͎
+A187 ID | SUBSCRIBE
+─────────────────┈ ❁۪۪
+
+╭┈──────RULES
+╰─➤⚒RULES📜
+↣TLPN=BLOCK+BAN
+↣SPAM=BAN
+↣NO PC
+↣JEDA 2 DETIK
+─────────────────┈ ❁۪۪
+╭┈─────FITUR
+╰─❁۪۪
+╰─➤ *${prefix}ownermenu*
+╰─➤ *${prefix}adminmenu*
+╰─➤ *${prefix}funmenu*
+╰─➤ *${prefix}mediamenu*
+╰─➤ *${prefix}kerangmenu*
+╰─➤ *${prefix}makermenu*
+╰─➤ *${prefix}othermenu*
+╰─➤ *${prefix}animemenu*
+╰─➤ *${prefix}nsfwmenu*
+╰─➤ *${prefix}vipmenu*
+─────────────────┈ ❁۪۪
+  ─➤ UMUM 
+─────────────────┈ ❁۪۪
+╰─➤ *${prefix}bugreport*
+╰─➤  *${prefix}info*
+╰─➤ *${prefix}owner*
+╰─➤ *${prefix}request*
+╰─➤ *${prefix}setprefix*
+╰─➤ *${prefix}listblock*
+╰─➤ *${prefix}iklan*
+╰─➤ *${prefix}runtime*
+╰─➤ *${prefix}rules*
+╰─➤ *${prefix}tnc*
+╰─➤ *${prefix}cekvip*
+╰─➤ *${prefix}daftarvip*
+╰─➤ *${prefix}addcip*
+╰─➤ *${prefix}dellvip*
+╰─➤ *${prefix}snk*
+╰─➤ *${prefix}listpremium*
+╰─➤ *${prefix}donate*
+╰─➤ *${prefix}fitnah*
+╰─➤ *${prefix}totaluser*
+╰─➤ *${prefix}level*
+╰─➤ *${prefix}leveling*
+╰─➤ *${prefix}glass*
+─────────────────┈ ❁۪۪
+─➤ UPDATE PADA 26-01-2021
+─➤ Fitur ini membutuhkan api,minat?
+ ╰➤ PC 085722553839「pesan ga penting block」
+─────────────────┈ ❁۪۪
+╰─➤ *${prefix}apiteks [text]*
+╰─➤ *${prefix}airteks [text]*
+╰─➤ *${prefix}metalteks [text]*
+╰─➤ *${prefix}mlogo [text]*
+╰─➤ *${prefix}ffbaner [text]*
+╰─➤ *${prefix}embun [text]*
+╰─➤ *${prefix}kunciteks [text]*
+╰─➤ *${prefix}say*
+╰─➤ *${prefix}addsay [text]*
+╰─➤ *${prefix}listsay*
+╰─➤ *${prefix}bacot*
+╰─➤ *${prefix}addbacot [text]*
+╰─➤ *${prefix}liatbacot*
+╰─➤ *${prefix}resetbacot*
+╰─➤ *${prefix}aguse*
+╰─➤ *${prefix}kicktime*
+╰─➤ *${prefix}nobg*
+╰─➤ *${prefix}url2img*
+╭┈─────────────────┈ ❁۪۪
+╰─➤Special Thanks To
+MhankBarBar
+Aruga
+Adiwajshing
+XPTN
+TOBZ
+A187ID
+CALIPH
+MASLENT
+─────────────────┈ ❁۪۪
+
+          ║▌│█║▌│ █║▌│█│║▌║
+          ║▌│█║▌│ █║▌│█│║▌║
+   
+             ©️ *ARIS187 ID*
+
+`
+}
+exports.help = help
